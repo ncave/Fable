4444b30e46c3fd4112d673ee17c3965bf1433ead
ncave <test2@hotmail.com>
2017-01-17 01:20:21 -0800

    [WIP] Fable support

---
+++ src/netcore/Fable.Client.Browser/app.fs
@@ -0,0 +1,37 @@
+module App
+
+open Microsoft.FSharp.Compiler.SourceCodeServices
+open Fable.Client
+
+#if !DOTNETCORE && !DOTNET40
+
+[<Fable.Core.Import("readFileSync", "fs")>]
+let readFileSync: System.Func<string, byte[]> = failwith "JS only"
+
+#else
+
+[<EntryPoint>]
+#endif
+let main argv =
+    try
+        let source = """
+type BB() = member x.MMM() = 2 * 3
+printfn "answer: %A" (42 * BB().MMM())
+"""
+        let metadataPath = "/temp/metadata/"
+        let references = ["mscorlib";"System";"System.Core";"System.Data";"System.IO";"System.Xml";"System.Numerics"]
+#if DOTNETCORE || DOTNET40
+        let readAllBytes = fun (fileName:string) -> System.IO.File.ReadAllBytes (metadataPath + fileName)
+#else
+        let readAllBytes = fun (fileName:string) -> readFileSync.Invoke (metadataPath + fileName)
+#endif
+
+        let checker = InteractiveChecker(references, readAllBytes)
+        let opts = readOptions argv
+        let com = makeCompiler opts []
+        let fileName = "stdin.fsx"
+        let success = compile com checker (fileName, source)
+        ()
+    with ex ->
+        printException ex
+    0
